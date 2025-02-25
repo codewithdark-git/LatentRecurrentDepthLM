2a6754dbf95524ba0ffc72a327948fccd35d669d
Dark Coder <codewithdark90@gmail.com>
2025-02-25 07:20:56 +0500

    Create locally.py

---
+++ Inference/locally.py
@@ -0,0 +1,13 @@
+# you can the save model also here like this 
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from Model.model import LatentRecurrentDepthModel
+
+def generator(prompt, num_interations=3, max_length=50, temperature=0.7):
+    tokenizer = AutoTokenizer.from_pretrained("codewithdark/latent-recurrent-depth-lm")
+    model = LatentRecurrentDepthModel.from_pretrained("codewithdark/latent-recurrent-depth-lm")
+
+    # prompt = "In the realm of language modeling"
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+    generated_ids = model.generate(input_ids, max_length=max_length, num_iterations=num_interations, temperature=temperature, top_k=50)
+    generated_text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+    return generated_text
